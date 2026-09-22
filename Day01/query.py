5f2a7711353b5d4475e9b6c256b8fb59b5353ee8
RANADEEPENTHALA <ranadeepenthala29708@gmail.com>
2026-09-22 11:23:55 +0530

    Day 1 ran a python script to run ai query

---
+++ Day01/query.py
@@ -0,0 +1,12 @@
+import ollama
+response=ollama.chat
+(
+    model="llama3.2:3b",
+    message=[
+        {
+            "role":"user",
+            "content":"Explain machine learning in 40 words"
+        }
+    ]
+)
+print(response["message"]["content"])
