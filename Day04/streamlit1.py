edbd315d668eb917290d899e55785afcf2252d9c
spoorthienthala <spoorthienthala@gmail.com>
2026-09-26 11:46:24 +0530

    Day05:uiBot

--- Day04/streamlit1.py
+++ Day04/streamlit1.py
@@ -3,6 +3,5 @@ st.title("My First Streamlit App!!!")
 st.write("Welcome to my AI application!")
 name=st.text_input("Enter your name:")
 age=st.number_input("Enter your age:")
-if st.button("Submit"):
-    st.write("Hello",name)
-    st.write("age:",age)
+st.write("Hello",name)
+st.write("age:",age)
--- uiBot.py
+++ uiBot.py
@@ -1,64 +1,46 @@
 import ollama
 import streamlit as st
-
 st.title("Welcome to my Chatbot App!!!")
-
 if "messages" not in st.session_state:
     st.session_state.messages = []
-
 with st.sidebar:
     st.header("File Upload")
-
     uploaded_file = st.file_uploader(
         "Upload a text file",
-        type=["txt"]
+        type=["text"]
     )
-
     if uploaded_file is not None:
         content = uploaded_file.read().decode("utf-8")
-
         st.success("File uploaded!")
-
         st.write("File name:", uploaded_file.name)
-
         with st.expander("View File Content"):
             st.write(content)
-
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
-
 question = st.chat_input("You:")
-
 if question:
     st.session_state.messages.append({
         "role": "user",
         "content": question
     })
-
     with st.chat_message("user"):
         st.write(question)
-
     messages = st.session_state.messages.copy()
-
     if uploaded_file is not None:
         messages.insert(0, {
             "role": "system",
             "content": "Use this file content to answer the user's questions:\n" + content
         })
-
     with st.spinner("AI is thinking...", show_time=True):
         response = ollama.chat(
             model="llama3.2:3b",
             messages=messages
         )
-
     answer = response["message"]["content"]
-
     st.session_state.messages.append({
         "role": "assistant",
         "content": answer
     })
-
     with st.chat_message("assistant"):
         st.write(answer)
